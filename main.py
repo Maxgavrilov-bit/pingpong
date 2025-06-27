42d611ad7448fd102c2eae055649d26d12d7f94b
Maxgavrilov-bit <maxgavrilovandr@gmail.com>
2025-06-27 19:50:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,27 +18,43 @@ class Player(GameSprite):
         self.count = 0
         self.hp = 3
     def updater(self):
-        if keys_pressed[K_UP] and self.rect.y > 49:
+        if keys_pressed[K_UP] and self.rect.y > 10:
             self.rect.y -= self.speed
-        if keys_pressed[K_DOWN] and self.rect.y < 450:
+        if keys_pressed[K_DOWN] and self.rect.y < 400:
             self.rect.y += self.speed
     def updatel(self):
-        if keys_pressed[K_W] and self.rect.y > 10:
+        if keys_pressed[K_w] and self.rect.y > 10:
             self.rect.y -= self.speed
-        if keys_pressed[K_S] and self.rect.y < 600:
+        if keys_pressed[K_s] and self.rect.y < 400:
             self.rect.y += self.speed
 
+
+class Ball(GameSprite):
+    def __init__(self, image_name, speed, x, y, width, length):
+        super().__init__( image_name, speed, x, y, width, length)
+    def update(self):
+        pass
+
+    
+ball1 = Ball('ball.png' , 15, 350, 250, 50, 50)
+platform1 = Player('i.webp' , 10, 40, 200, 100, 30)
+platform2 = Player('i.webp' , 10, 640, 200, 100, 30)
 clock = time.Clock()
 FPS = 60
 window = display.set_mode((700, 500))
 display.set_caption('ping pong')
-window.fill((5, 30, 200))
-final = False
-while final != True:
+window.fill((10, 200, 242))
+game = True
+while game:
     clock.tick(FPS)
     display.update()
-    
+    window.fill((10, 200, 242))
     keys_pressed = key.get_pressed()
     for e in event.get():
         if e.type == QUIT:
             game = False
+    platform1.reset()
+    platform2.reset()
+    platform1.updatel()
+    platform2.updater()
+    ball1.reset()
